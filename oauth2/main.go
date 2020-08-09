ba8b6e5b956b386fbecb4d0fc828e6b60ba17bb4
pong.wu <pong.wu@sunvalley.com.cn>
2020-08-09 16:26:35 +0800

    implement oauth2 in go

---
+++ oauth2/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+const clientID = "7e42e75337975fb441a5"
+const clientSecret = ""
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+
+	// We will be using `httpClient` to make external HTTP requests later in our code
+	httpClient := http.Client{}
+
+	// Create a new redirect route route
+	http.HandleFunc("/oauth/redirect", func(w http.ResponseWriter, r *http.Request) {
+		// First, we need to get the value of the `code` query param
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse query: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		code := r.FormValue("code")
+
+		// Next, lets for the HTTP request to call the github oauth enpoint
+		// to get our access token
+		reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
+		req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		// We set this header since we want the response
+		// as JSON
+		req.Header.Set("accept", "application/json")
+
+		// Send out the HTTP request
+		res, err := httpClient.Do(req)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		defer res.Body.Close()
+
+		// Parse the request body into the `OAuthAccessResponse` struct
+		var t OAuthAccessResponse
+		if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		// Finally, send a response to redirect the user to the "welcome" page
+		// with the access token
+		//w.Header().Set("Location", "/welcome.html?access_token="+t.AccessToken)
+		//w.WriteHeader(http.StatusFound)
+
+		reqURL = fmt.Sprintf("https://api.github.com/user")
+		req, err = http.NewRequest(http.MethodGet, reqURL, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		req.Header.Set("accept", "application/json")
+		req.Header.Set("Authorization", "token "+t.AccessToken)
+
+		// Send out the HTTP request
+		res, err = httpClient.Do(req)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		defer res.Body.Close()
+
+		// Parse the request body into the `OAuthAccessResponse` struct
+		var userInfo ServiceProviderResponse
+		if err := json.NewDecoder(res.Body).Decode(&userInfo); err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		w.Write([]byte("hello" + userInfo.Login))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type OAuthAccessResponse struct {
+	AccessToken string `json:"access_token"`
+}
+type ServiceProviderResponse struct {
+	Login string `json:"login"`
+}
